9d6eaf355c30af019d60382b09f4d3aa39e44b52
Zach <zjonesengineering@gmail.com>
2015-07-04 22:31:07 -0700

    added error checking and more complete demo

--- pov.py
+++ pov.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+import os
 
 characters = {
     'A' : ['0xE0','0xDB','0xBB','0xDB','0xE0'],
@@ -30,7 +32,9 @@ characters = {
 }
 
 header = """\
-//This sketch was created using writePOV.m to fill the I2C EEPROM on the POV fan
+//This sketch was created using the pov.py module to 
+//fill the I2C EEPROM on the POV fan
+//
 //LED on Pin 13 blinks once data transfer is complete
 //
 //Connections required:
@@ -65,28 +69,75 @@ footer = """\
 }
 """
 
-class POV():
-    
-    def __init__(self, screens, filename='pov_sketch.pde'):
+class POV():    
+    """
+    :param screens: Screen contents
+    :type screens: list of strings
+    :param name: Folder/File name for Arduino sketch
+    :type name: string
+    """        
+    def __init__(self, screens, name='pov_sketch'):
+        self.screens = screens
+        self.name = name     
+        self.check_inputs()
+        
         self.address_counter = 0
         self.i2c_address = '0x50'
         
-        self.sketch = open(filename, 'w')
+        if not os.path.isdir(name):
+            os.mkdir(name)
+        
+        self.sketch = open(name + '/' + name + '.ino', 'w')
         self.sketch.write(header)
         self.sketch.write('  Wire.beginTransmission(' + str(self.i2c_address) +  ');\n')
         self.sketch.write('  Wire.write(' + str(self.address_counter) + ');\n')
         self.sketch.write('  Wire.write(' + str(len(screens)) + ');\n')
+        self.increment_address()
         
-        for screen in screens:
+        for screen in self.screens:
             self.write_screen(screen)
         
         self.sketch.write(footer)
         self.sketch.close()
 
+    def check_inputs(self):
+        #Convert all characters to uppercase
+        for idx,screen in enumerate(self.screens):
+            self.screens[idx] = screen.upper()        
+        
+        #Drop screens with invalid characters
+        for screen in self.screens:
+            try:
+                for letter in screen:
+                    characters[letter]
+            except KeyError:
+                print('Bad character: ' + letter)
+                print('\tDropping screen: ' + screen)
+                self.screens.remove(screen)
+        
+        #Drop screens with too many characters
+        for screen in self.screens:
+            if len(screen) > 20:
+                print('Max number of letters is 20')
+                print('\tDropping screen: ' + screen)
+                self.screens.remove(screen)
+        
+        #Drop screens if too many provided
+        if len(self.screens) > 6:
+            print('Max number of screens is 6')
+            while len(self.screens) > 6:
+                print('\tDropping screen: ' + self.screens.pop())
+        
+        print('Valid Screens:')
+        print(self.screens)
+
     def increment_address(self):
         self.address_counter += 1
         if self.address_counter >= 256:
             self.i2c_address = '0x51'
+        if self.address_counter >= 511:
+            print('Error: Too much data...shorten messages')
+            raise(Exception)
     
     def check_page_break(self):
         if self.address_counter % 16 == 0:
@@ -102,7 +153,7 @@ class POV():
         self.check_page_break()
         for letter in reversed(screen):
             self.sketch.write('  //' + '-'*10 + letter + '-'*10 +  '\n')
-            for character in characters[letter.upper()]:
+            for character in characters[letter]:
                 self.sketch.write('  Wire.write(' + character + ');\n')
                 self.increment_address()
                 self.check_page_break()
