36b4d7123e0634fe89280e13c7314220eb2b9af7
Boris <borisc@dmcinfo.com>
2016-11-22 21:38:00 -0700

    I apparently deleted a few files from the original branch. Reverting.

--- demo_pov.py
+++ demo_pov.py
@@ -4,14 +4,10 @@ POV(
     screens = [
         'TESTING POV FAN',
         'NEW SOFTWARE UPDATE',
-        'capitalize lowercase', #This line will become uppercase
-        'Invalid Character #', #This line will be omitted
+        'Lowercase string',
         'PYTHON POWERED',
-        'Too many characters in this string', #This line will be omitted
         'ZJEMBEDDED',
         'BLOGSPOT',
-        'Extra screen one', #This line will be omitted
-        'Extra screen two', #This line will be omitted
     ],
     name = 'demo_pov_sketch'
 )
